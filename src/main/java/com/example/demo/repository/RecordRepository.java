cd739b524abb099055d40502b5f49f25eb41d231
dilik14 <dilik1407@yandex.by>
2024-12-08 20:17:17 +0300

    Rename record to task record

--- src/main/java/com/example/demo/repository/RecordRepository.java
+++ src/main/java/com/example/demo/repository/RecordRepository.java
@@ -1,8 +1,8 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Record;
+import com.example.demo.model.TaskRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface RecordRepository extends JpaRepository<Record, Long> {
+public interface RecordRepository extends JpaRepository<TaskRecord, Long> {
 }
