7af0a12a0b80e83cb83dcb7d1426e1be2174a2f3
dilik14 <dilik1407@yandex.by>
2024-12-08 21:05:25 +0300

    Unused import 🤓

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -17,8 +17,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 @Controller
 public class AuthController {
